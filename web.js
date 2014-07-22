25a6d1c6141ae93a7c6d00e30ced5b8f7e6cfeac
Paolo Burelli <paolo@yamaneko.lan>
2014-07-23 00:26:13 +0200

    second attempt

--- web.js
+++ web.js
@@ -1,14 +1,17 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
 var app = express();
 
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get('/list/:name', function(req, res){
+  res.send('list: ' + req.params.name);
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.delete('/list/:name', function(req, res){
+  res.send('deleted list: ' + req.params.name);
 });
+
+app.put('/list/:name/item/:itemname', function(req, res){
+  res.send('added item '+ req.params.itemname+' to list: ' + req.params.name);
+});
+
+app.listen(80);
+console.log('Listening on port 80');
