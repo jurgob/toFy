29af157d0ca39265d0869b1c30f23695a2d73a6e
Paolo Burelli <paolo@yamaneko.lan>
2014-07-23 00:36:47 +0200

    switched to PRT

--- web.js
+++ web.js
@@ -13,5 +13,5 @@ app.put('/list/:name/item/:itemname', function(req, res){
   res.send('added item '+ req.params.itemname+' to list: ' + req.params.name);
 });
 
-app.listen(5000);
-console.log('Listening on port 5000');
+app.listen(process.env.PORT||5000);
+console.log('Listening on port' + process.env.PORT);
