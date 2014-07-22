20ff8a5f8bf75001f2eacd70fbb5c91e02ddf058
Paolo Burelli <paolo@yamaneko.lan>
2014-07-23 00:30:59 +0200

    switched to 5000

--- web.js
+++ web.js
@@ -13,5 +13,5 @@ app.put('/list/:name/item/:itemname', function(req, res){
   res.send('added item '+ req.params.itemname+' to list: ' + req.params.name);
 });
 
-app.listen(8080);
-console.log('Listening on port 8080');
+app.listen(5000);
+console.log('Listening on port 5000');
