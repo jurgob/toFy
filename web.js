386bdd068043cbaefd7db124ebda87cfff9a697a
Paolo Burelli <burelli@gmail.com>
2014-07-23 15:15:42 +0200

    Added logging facility

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var jf = require('jsonfile');
 var util = require('util');
+var fs = require('fs');
 
 var status = {
 	"ok":200,
@@ -26,9 +27,20 @@ function loadDB(){
 	jf.readFile(listsDB, function(err, obj) { lists = obj; });
 }
 
+function logRequest(req) {
+	fs.appendFile('data/log.csv',new Date().toString()+", "+req.ip+", "+req.method+", "+req.path,function (err){});
+}
+
+function logResponse(statusCode) {
+	fs.appendFile('data/log.csv',", "+statusCode+"\n",function (err){});
+}
+
+
 function r(statusCode,data){
 	if (typeof data === "undefined")
 		data = {}
+
+	logResponse(statusCode);
 	return {"status":statusCode,"data":data};
 }
 
@@ -56,7 +68,6 @@ function getNewPassword(req) {
 		return "";
 }
 
-
 function checkPassword(listName,req){
 	password = getPassword(req);
 	if (typeof lists[listName].password === "undefined" || lists[listName].password === null || lists[listName].password === "")
@@ -64,6 +75,12 @@ function checkPassword(listName,req){
 	else
 		return lists[listName].password === password;
 }
+
+app.all('*', function(req, res, next){
+	logRequest(req);
+	next();	
+});
+
 
 app.put('/api/v1/list/:name/password', function(req, res){
   if (listExists(req.params.name)){
@@ -74,10 +91,11 @@ app.put('/api/v1/list/:name/password', function(req, res){
 	} 
 	else res.json(r(status.unauthorized));
   } 
-  else res.json(r(status.notFound));
+  else res.json(r(status.preconditionFailed));
 });
 
-app.get('/api/v1/list/:name', function(req, res){
+app.route('/api/v1/list/:name')
+.get(function(req, res){
   if (listExists(req.params.name)){
 	if (checkPassword(req.params.name,req))
 		res.json(r(status.ok,{items:lists[req.params.name].items}));
@@ -86,9 +104,8 @@ app.get('/api/v1/list/:name', function(req, res){
   } else {
   	res.json(r(status.notFound));
   } 
-});
-
-app.delete('/api/v1/list/:name', function(req, res){
+})
+.delete(function(req, res){
   if (listExists(eq.params.name)){
 	if (checkPassword(req.params.name,req)){
 		delete(lists[req.params.name]);
@@ -100,9 +117,8 @@ app.delete('/api/v1/list/:name', function(req, res){
   } else {
   	res.json(r(status.notFound));
   } 
-});
-
-app.put('/api/v1/list/:name', function(req, res){
+})
+.put(function(req, res){
   if (!listExists(req.params.name)){
 	lists[req.params.name] = {"items":[],"password":getPassword(req)}; 
 	saveDB();
@@ -112,7 +128,8 @@ app.put('/api/v1/list/:name', function(req, res){
   } 
 });
 
-app.put('/api/v1/list/:name/item/:itemname', function(req, res){
+app.route('/api/v1/list/:name/item/:itemname') 
+.put(function(req, res){
   if (listExists(req.params.name)){
 	if (checkPassword(req.params.name,req)){
 	  if (!itemExists(req.params.name,req.params.itemname)){
@@ -127,9 +144,8 @@ app.put('/api/v1/list/:name/item/:itemname', function(req, res){
   } else {
   	res.json(r(status.preconditionFailed));
   } 
-});
-
-app.delete('/api/v1/list/:name/item/:itemname', function(req, res){
+})
+.delete(function(req, res){
 	if (listExists(req.params.name)){
 		if (checkPassword(req.params.name,req)){
 	  		if (itemExists(req.params.name,req.params.itemname)){
