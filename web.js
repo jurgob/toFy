94ed05eb67012cf7622818e0f917ce8861a6e54a
Paolo Burelli <paolo@Yamaneko.local>
2014-07-23 11:24:26 +0200

    added file based persistency

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var jf = require('jsonfile');
+var util = require('util');
 
 var status = {
 	"ok":200,
@@ -10,8 +12,19 @@ var status = {
 	"preconditionFailed":412
 }
 
-var lists = {"cestil":{"items":["cane","gatto","pollo"]}};
+var lists = {};
+var listsDB = 'data/lists.json';
 
+function saveDB(){
+	jf.writeFile(listsDB, lists, function(err) {
+		if (err != null)
+			console.log(err);
+	})
+}
+
+function loadDB(){
+	jf.readFile(listsDB, function(err, obj) { lists = obj; });
+}
 
 function r(statusCode,data){
 	if (typeof data === "undefined")
@@ -38,6 +51,7 @@ app.get('/api/v1/list/:name', function(req, res){
 app.delete('/api/v1/list/:name', function(req, res){
   if (listExists(eq.params.name)){
 	delete(lists[req.params.name]);
+	saveDB();
 	res.json(r(status.ok));
   } else {
   	res.json(r(status.notFound));
@@ -47,6 +61,7 @@ app.delete('/api/v1/list/:name', function(req, res){
 app.put('/api/v1/list/:name', function(req, res){
   if (!listExists(req.params.name)){
 	lists[req.params.name] = {items:[]};
+	saveDB();
 	res.json(r(status.ok));
   } else {
   	res.json(r(status.conflict));
@@ -57,6 +72,7 @@ app.put('/api/v1/list/:name/item/:itemname', function(req, res){
   if (listExists(req.params.name)){
 	  if (!itemExists(req.params.name,req.params.itemname)){
 		lists[req.params.name].items.push(req.params.itemname);		
+		saveDB();
 		res.json(r(status.ok,{"items":lists[req.params.name].items}));
   	  } else {
   		res.json(r(status.conflict));
@@ -71,6 +87,7 @@ app.delete('/api/v1/list/:name/item/:itemname', function(req, res){
 	  if (itemExists(req.params.name,req.params.itemname)){
   		var idx = lists[req.params.name].items.lastIndexOf(req.params.itemname);
 		lists[req.params.name].items.splice(idx,1);
+		saveDB();
 		res.json(r(status.ok,{"items":lists[req.params.name].items}));
 	  } else {
 		res.json(r(status.notFound));
@@ -84,6 +101,12 @@ app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
 });
 
+app.all('*', function(req, res){
+	res.json(r(status.bad));
+});
+
+
+loadDB();
 var port = Number(process.env.PORT || 3000);
 app.listen(port, function() {
   console.log("Listening on " + port);
