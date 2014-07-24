acc6586de9daaa40fabae013010514dc49e2714b
Paolo Burelli <burelli@gmail.com>
2014-07-24 11:08:14 +0200

    Added redis storage

--- web.js
+++ web.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var app = express();
-var jf = require('jsonfile');
 var util = require('util');
-var fs = require('fs');
+var zlib = require('zlib');
 
 var redis = require('redis');
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
-
-//client.set('foo', 'bar');
-//client.get('foo', function (err, reply) {
-//    console.log(reply.toString()); // Will print `bar`
-//});
+client.on("error", function (err) {
+        console.log("Redis: "+err);
+});
 
 var status = {
 	"ok":200,
@@ -24,30 +21,13 @@ var status = {
 	"preconditionFailed":412
 }
 
-var lists = {};
-var listsDB = __dirname + '/tmp/lists.json';
-var logFile = __dirname + '/tmp/log.csv';
-
-function saveDB(){
-	jf.writeFile(listsDB, lists, function(err) {
-		if (err != null)
-			console.log(err);
-	})
-}
-
-function loadDB(){
-	jf.readFile(listsDB, function(err, obj) { 
-		if (typeof obj != "undefined" && obj != null)
-			lists = obj;
-	});
-}
 
 function logRequest(req) {
-	fs.appendFile(logFile,new Date().toString()+", "+req.ip+", "+req.method+", "+req.path,function (err){});
+	console.log(new Date().toString()+", "+req.ip+", "+req.method+", "+req.path);
 }
 
 function logResponse(statusCode) {
-	fs.appendFile(logFile,", "+statusCode+"\n",function (err){});
+	console.log(statusCode);
 }
 
 
@@ -59,12 +39,28 @@ function r(statusCode,data){
 	return {"status":statusCode,"data":data};
 }
 
-function listExists(listName) {
-	return typeof lists[listName] === "object";
+function getList(listName,callback){
+	client.get(listName, function (err,reply){
+		if (reply != null){
+			zlib.inflate(new Buffer(reply.toString(), 'base64'),function (err,output){	callback(JSON.parse(output.toString('utf8')));	});
+		} else
+			callback(null);
+	});
 }
 
-function itemExists(listName,itemName) {
-	return lists[listName].items.lastIndexOf(itemName) != -1;
+function setList(listName,list){
+	zlib.deflate(JSON.stringify(list),function (err,buffer) {
+		client.set(listName,buffer.toString('base64'));
+	});
+}
+
+function deleteList(listName) {
+	client.del(listName);
+}
+
+
+function itemExists(list,itemName) {
+	return list.items.lastIndexOf(itemName) != -1;
 }
 
 function getPassword(req) {
@@ -83,12 +79,12 @@ function getNewPassword(req) {
 		return "";
 }
 
-function checkPassword(listName,req){
+function checkPassword(list,req){
 	password = getPassword(req);
-	if (typeof lists[listName].password === "undefined" || lists[listName].password === null || lists[listName].password === "")
+	if (typeof list.password === "undefined" || list.password === null || list.password === "")
 		return true;
 	else
-		return lists[listName].password === password;
+		return list.password === password;
 }
 
 app.all('*', function(req, res, next){
@@ -98,82 +94,87 @@ app.all('*', function(req, res, next){
 
 
 app.put('/api/v1/list/:name/password', function(req, res){
-  if (listExists(req.params.name)){
-	if (checkPassword(req.params.name,req)){
-		lists[req.params.name]["password"] =  getNewPassword(req);
-		saveDB();
-		res.json(r(status.ok));
-	} 
-	else res.json(r(status.unauthorized));
-  } 
-  else res.json(r(status.preconditionFailed));
+  getList(req.params.name, function(list) {
+
+  	if (list != null){
+		if (checkPassword(list,req)){
+			list.password =  getNewPassword(req);
+			setList(req.params.name,list);
+			res.json(r(status.ok));
+		} 
+		else res.json(r(status.unauthorized));
+  	} 
+  	else res.json(r(status.preconditionFailed));
+
+  });
 });
 
 app.route('/api/v1/list/:name')
 .get(function(req, res){
-  if (listExists(req.params.name)){
-	if (checkPassword(req.params.name,req))
-		res.json(r(status.ok,{items:lists[req.params.name].items}));
-	else
-		res.json(r(status.unauthorized));
-  } else {
-  	res.json(r(status.notFound));
-  } 
+	getList(req.params.name, function(list) {
+		if (list != null){
+			if (checkPassword(list,req))
+				res.json(r(status.ok,{"items":list.items}));
+			else res.json(r(status.unauthorized));
+  		} 
+		else res.json(r(status.notFound));
+  	}); 
 })
 .delete(function(req, res){
-  if (listExists(eq.params.name)){
-	if (checkPassword(req.params.name,req)){
-		delete(lists[req.params.name]);
-		saveDB();
-		res.json(r(status.ok));
-	} else
-		res.json(r(status.unauthorized));
-
-  } else {
-  	res.json(r(status.notFound));
-  } 
+	getList(req.params.name, function(list) {
+ 		if (list != null){
+			if (checkPassword(list,req)){
+				deleteList(req.params.name);		
+				res.json(r(status.ok));
+			} 
+			else res.json(r(status.unauthorized));
+  		} 
+		else res.json(r(status.notFound));
+	});
 })
 .put(function(req, res){
-  if (!listExists(req.params.name)){
-	lists[req.params.name] = {"items":[],"password":getPassword(req)}; 
-	saveDB();
-	res.json(r(status.ok));
-  } else {
-  	res.json(r(status.conflict));
-  } 
+	getList(req.params.name, function(list) {
+		if (list === null){
+			setList(req.params.name,{"items":[],"password":getPassword(req)}); 
+			res.json(r(status.ok));
+  		} 
+		else res.json(r(status.conflict));
+  	}); 
 });
 
 app.route('/api/v1/list/:name/item/:itemname') 
 .put(function(req, res){
-  if (listExists(req.params.name)){
-	if (checkPassword(req.params.name,req)){
-	  if (!itemExists(req.params.name,req.params.itemname)){
-		lists[req.params.name].items.push(req.params.itemname);		
-		saveDB();
-		res.json(r(status.ok,{"items":lists[req.params.name].items}));
-  	  } else {
-  		res.json(r(status.conflict));
-	  } 
-	} else
-		res.json(r(status.unauthorized));
-  } else {
-  	res.json(r(status.preconditionFailed));
-  } 
+	getList(req.params.name, function(list) {
+		if (list != null){
+			if (checkPassword(list,req)){
+	  			if (!itemExists(list,req.params.itemname)){
+					list.items.push(req.params.itemname);		
+					setList(req.params.name,list);
+					res.json(r(status.ok,{"items":list.items}));
+  	  			} 
+				else res.json(r(status.conflict));
+			} 
+			else res.json(r(status.unauthorized));
+  		} 
+		else res.json(r(status.preconditionFailed));
+	});
 })
 .delete(function(req, res){
-	if (listExists(req.params.name)){
-		if (checkPassword(req.params.name,req)){
-	  		if (itemExists(req.params.name,req.params.itemname)){
-  				var idx = lists[req.params.name].items.lastIndexOf(req.params.itemname);
-				lists[req.params.name].items.splice(idx,1);
-				saveDB();
-				res.json(r(status.ok,{"items":lists[req.params.name].items}));
-	  		} 
-			else res.json(r(status.notFound));
-		} 
-		else res.json(r(status.unauthorized));
-  	} 
-	else res.json(r(status.preconditionFailed));
+	getList(req.params.name, function(list) {
+		if (list != null){
+			if (checkPassword(list,req)){
+	  			if (itemExists(list,req.params.itemname)){
+  					var idx = list.items.lastIndexOf(req.params.itemname);
+					list.items.splice(idx,1);
+					setList(req.params.name,list);
+					res.json(r(status.ok,{"items":list.items}));
+	  			} 
+				else res.json(r(status.notFound));
+			} 
+			else res.json(r(status.unauthorized));
+  		} 
+		else res.json(r(status.preconditionFailed));
+	});
 });
 
 app.get('/', function(req, res){
@@ -185,7 +186,6 @@ app.all('*', function(req, res){
 });
 
 
-loadDB();
 var port = Number(process.env.PORT || 3000);
 app.listen(port, function() {
   console.log("Listening on " + port);
