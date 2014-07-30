530b860205e2dcbdbd72316b6bf6395f20521d8d
Paolo Burelli <burelli@gmail.com>
2014-07-30 16:40:58 +0200

    removed db deletion

--- web.js
+++ web.js
@@ -36,10 +36,10 @@ var events = {
 var openConnections = {};
 
 //Clean all the keys
-client.keys("*", function(err, key) {
-  client.del(key, function(err) {
-  });
-});
+//client.keys("*", function(err, key) {
+//  client.del(key, function(err) {
+//  });
+//});
 
 function logRequest(req) {
 	console.log(new Date().toString()+", "+req.ip+", "+req.method+", "+req.path);
